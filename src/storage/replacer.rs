774e1ae365b8566781d0ba4216d120b5bbc8750c
YBryzhan <gosha.b.dev@gmail.com>
2025-10-31 18:31:52 -0400

    feat: lruk replacer algo tests

--- src/storage/replacer.rs
+++ src/storage/replacer.rs
@@ -78,8 +78,10 @@ impl LRUKReplacer {
                 }
             }
         }
-        // No evictable frame found
-        candidate.frame_id
+
+        candidate
+            .frame_id
+            .inspect(|frame_id| self.remove(*frame_id))
     }
 
     pub fn record_access(&mut self, frame_id: usize) {
@@ -131,5 +133,94 @@ impl LRUKReplacer {
 
     pub fn size(&self) -> usize {
         self.curr_size as usize
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lruk_replacer() {
+        let mut replacer = LRUKReplacer::new(7, 2);
+
+        replacer.record_access(1);
+        replacer.record_access(2);
+        replacer.record_access(3);
+        replacer.record_access(4);
+        replacer.record_access(5);
+        replacer.record_access(6);
+
+        replacer.set_evictable(1, true);
+        replacer.set_evictable(2, true);
+        replacer.set_evictable(3, true);
+        replacer.set_evictable(4, true);
+        replacer.set_evictable(5, true);
+        replacer.set_evictable(6, false);
+
+        assert_eq!(replacer.size(), 5);
+
+        // Frame 1 now has 2 accesses
+        replacer.record_access(1);
+
+        // All other frames now share the maximum backward k-distance. Since we use timestamps to break ties, where the first
+        // to be evicted is the frame with the oldest timestamp, the order of eviction should be [2, 3, 4, 5, 1].
+        assert_eq!(replacer.evict(), Some(2));
+        assert_eq!(replacer.evict(), Some(3));
+        assert_eq!(replacer.evict(), Some(4));
+        assert_eq!(replacer.size(), 2);
+        // Now the replacer has the frames [5, 1].
+
+        // Insert new frames [3, 4], and update the access history for 5. Now, the ordering is [3, 1, 5, 4].
+        replacer.record_access(3);
+        replacer.record_access(4);
+        replacer.record_access(5);
+        replacer.record_access(4);
+        replacer.set_evictable(3, true);
+        replacer.set_evictable(4, true);
+
+        assert_eq!(replacer.size(), 4);
+        assert_eq!(replacer.evict(), Some(3));
+        assert_eq!(replacer.size(), 3);
+
+        // Set 6 to be evictable. 6 should be evicted next since it has the maximum backward k-distance.
+        replacer.set_evictable(6, true);
+        assert_eq!(replacer.size(), 4);
+        assert_eq!(replacer.evict(), Some(6));
+        assert_eq!(replacer.size(), 3);
+
+        // Mark frame 1 as non-evictable. We now have [5, 4].
+        replacer.set_evictable(1, false);
+        assert_eq!(replacer.size(), 2);
+        assert_eq!(replacer.evict(), Some(5));
+        assert_eq!(replacer.size(), 1);
+
+        replacer.record_access(1);
+        replacer.record_access(1);
+        replacer.set_evictable(1, true);
+
+        assert_eq!(replacer.evict(), Some(4));
+        assert_eq!(replacer.evict(), Some(1));
+        assert_eq!(replacer.size(), 0);
+
+        // Insert frame 1 again and mark it as non-evictable.
+        replacer.record_access(1);
+        replacer.set_evictable(1, false);
+        assert_eq!(replacer.size(), 0);
+
+        assert_eq!(replacer.evict(), None);
+        replacer.set_evictable(1, true);
+
+        assert_eq!(replacer.evict(), Some(1));
+        assert_eq!(replacer.size(), 0);
+
+        // There is nothing left in the replacer, so make sure this doesn't do something strange.
+        let frame_id = replacer.evict();
+        assert_eq!(frame_id, None);
+        assert_eq!(replacer.size(), 0);
+
+        replacer.set_evictable(6, true);
+        assert_eq!(replacer.size(), 0);
+        replacer.set_evictable(6, false);
     }
 }
